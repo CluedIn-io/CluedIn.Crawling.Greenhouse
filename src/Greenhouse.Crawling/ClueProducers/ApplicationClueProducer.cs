1d55ceac6661d1ce7bb943168430eba614fddf52
Tim D. Ward <tiw@cluedin.com>
2021-03-19 16:43:21 +1000

    Adding in the Application Clue Producer

--- src/Greenhouse.Crawling/ClueProducers/ApplicationClueProducer.cs
+++ src/Greenhouse.Crawling/ClueProducers/ApplicationClueProducer.cs
@@ -6,6 +6,8 @@ using CluedIn.Crawling.Greenhouse.Core.Models;
 using CluedIn.Crawling.Greenhouse.Infrastructure;
 using Microsoft.Extensions.Logging;
 using System.Linq;
+using CluedIn.Crawling.Helpers;
+using CluedIn.Crawling.Greenhouse.Vocabularies;
 
 namespace CluedIn.Crawling.Greenhouse.ClueProducers
 {
@@ -25,97 +27,20 @@ namespace CluedIn.Crawling.Greenhouse.ClueProducers
             var clue = _factory.Create("/Application", input.Id.ToString(), id);
             var data = clue.Data.EntityData;
 
-            //var vocab = new CandidateVocabulary();
+            var vocab = new ApplicationVocabulary();
 
-            //if (!string.IsNullOrEmpty(input.FirstName) && !string.IsNullOrEmpty(input.LastName))
-            //{
-            //    data.Name = string.Format("{0}{1}", input.FirstName, input.LastName);
-            //}
+            data.Properties[vocab.Status] = input.Status.PrintIfAvailable();
+            data.Properties[vocab.RejectionReason] = input.RejectionReason.PrintIfAvailable();
+            data.Properties[vocab.RejectionDetails] = input.RejectionDetails.PrintIfAvailable();
+            data.Properties[vocab.RejectedAt] = input.RejectedAt.ToString().PrintIfAvailable();
+            data.Properties[vocab.ProspectiveOffice] = input.ProspectiveOffice.PrintIfAvailable();
+            data.Properties[vocab.ProspectiveDepartment] = input.ProspectiveDepartment.PrintIfAvailable();
 
-            ////if (!string.IsNullOrEmpty(input.Description))
-            ////{
-            ////    data.Description = input.Description;
-            ////}
-
-            //DateTimeOffset modifiedDate;
-            //if (DateTimeOffset.TryParse(input.UpdatedAt.ToString(), out modifiedDate))
-            //{
-            //    data.ModifiedDate = modifiedDate;
-            //}
-
-            //DateTimeOffset createdDate;
-            //if (DateTimeOffset.TryParse(input.CreatedAt.ToString(), out createdDate))
-            //{
-            //    data.CreatedDate = createdDate;
-            //}
-
-            //if (input.Tags != null)
-            //    foreach (var tag in input.Tags)
-            //    {
-            //        data.Tags.Add(new Tag(tag.ToString()));
-            //    }
-
-            //if (input.SocialMediaAddresses != null)
-            //    foreach (var socialMediaAddress in input.SocialMediaAddresses)
-            //    {
-            //        data.Aliases.Add(socialMediaAddress.ToString());
-            //    }
-
-            //if (input.PhoneNumbers != null)
-            //    foreach (var phoneNumbers in input.PhoneNumbers)
-            //    {
-            //        data.Aliases.Add(phoneNumbers.ToString());
-            //    }
-
-            //if (input.EmailAddresses != null)
-            //    foreach (var emailAddress in input.EmailAddresses)
-            //    {
-            //        var code = new EntityCode("/Candidate", "CluedIn", emailAddress.Value);
-            //        data.Codes.Add(code);
-            //    }
-
-            //if (input.Educations != null)
-            //    foreach (var education in input.Educations)
-            //    {
-            //        _factory.CreateOutgoingEntityReference(clue, "/School", EntityEdgeType.Attended, education, education.Id.ToString());
-            //    }
-
-            ////if (input.Employments != null)
-            ////    foreach (var employment in input.Employments)
-            ////    {
-            ////        _factory.CreateOutgoingEntityReference(clue, "/Placement", EntityEdgeType.Attended, employment, employment.Id.ToString());
-            ////    }
-
-            //if (input.Company != null)
-            //    _factory.CreateOutgoingEntityReference(clue, EntityType.Organization, EntityEdgeType.Attended, input, input.Company);
-
-            //if (input.Attachments != null)
-            //    foreach (var attachment in input.Attachments)
-            //    {
-            //        //You might need to parse this. 
-            //        _factory.CreateOutgoingEntityReference(clue, EntityType.Files.File, EntityEdgeType.PartOf, attachment, attachment.Url.ToString());
-            //    }
-
-            //if (input.Applications != null)
-            //    foreach (var application in input.ApplicationIds)
-            //    {
-            //        _factory.CreateOutgoingEntityReference(clue, "/Application", EntityEdgeType.For, application, application.ToString());
-            //    }
-
-            //if (input.Addresses != null)
-            //    foreach (var address in input.Addresses)
-            //    {
-            //        _factory.CreateOutgoingEntityReference(clue, EntityType.Location, EntityEdgeType.For, address, address.Value.ToString());
-            //    }
-
-            //data.Properties[vocab.Title] = input.Title.PrintIfAvailable();
-            //data.Properties[vocab.PhotoUrl] = input.PhotoUrl.PrintIfAvailable();
-            //data.Properties[vocab.LastName] = input.LastName.PrintIfAvailable();
-            //data.Properties[vocab.LastActivity] = input.LastActivity.ToString().PrintIfAvailable();
-            //data.Properties[vocab.Coordinator] = input.Coordinator.PrintIfAvailable();
-            //data.Properties[vocab.CanEmail] = input.CanEmail.PrintIfAvailable();
-
-            //data.Properties[vocab.FirstName] = input.FirstName.PrintIfAvailable();
+            data.Properties[vocab.Prospect] = input.Prospect.PrintIfAvailable();
+            data.Properties[vocab.Location] = input.Location.PrintIfAvailable();
+            data.Properties[vocab.LastActivityAt] = input.LastActivityAt.PrintIfAvailable();
+            data.Properties[vocab.CandidateId] = input.CandidateId.ToString().PrintIfAvailable();
+            data.Properties[vocab.AppliedAt] = input.AppliedAt.PrintIfAvailable();
 
 
             if (!data.OutgoingEdges.Any())
---
+++ src/Greenhouse.Crawling/Vocabularies/ApplicationVocabulary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CluedIn.Core.Data.Vocabularies;
+
+
+namespace CluedIn.Crawling.Greenhouse.Vocabularies
+{
+    public class ApplicationVocabulary : SimpleVocabulary
+    {
+        public ApplicationVocabulary()
+        {
+            VocabularyName = "Greenhouse Application"; // TODO: Set value
+            KeyPrefix = "greenhouse.Application"; // TODO: Set value
+            KeySeparator = ".";
+            Grouping = "/Application"; // TODO: Check value
+
+            AddGroup("Greenhouse Details", group =>
+            {
+                RejectionReason = group.Add(new VocabularyKey("RejectionReason", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Status = group.Add(new VocabularyKey("Status", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                RejectionDetails = group.Add(new VocabularyKey("RejectionDetails", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                RejectedAt = group.Add(new VocabularyKey("RejectedAt", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                ProspectiveOffice = group.Add(new VocabularyKey("ProspectiveOffice", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                ProspectiveDepartment = group.Add(new VocabularyKey("ProspectiveDepartment", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Prospect = group.Add(new VocabularyKey("Prospect", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Location = group.Add(new VocabularyKey("Location", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                LastActivityAt = group.Add(new VocabularyKey("LastActivityAt", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                CandidateId = group.Add(new VocabularyKey("CandidateId", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                AppliedAt = group.Add(new VocabularyKey("AppliedAt", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+            });
+
+        }
+
+        public VocabularyKey RejectionReason { get; internal set; }
+        public VocabularyKey Status { get; internal set; }
+        public VocabularyKey RejectionDetails { get; internal set; }
+        public VocabularyKey RejectedAt { get; internal set; }
+        public VocabularyKey ProspectiveOffice { get; internal set; }
+        public VocabularyKey ProspectiveDepartment { get; internal set; }
+        public VocabularyKey Prospect { get; internal set; }
+        public VocabularyKey Location { get; internal set; }
+        public VocabularyKey LastActivityAt { get; internal set; }
+        public VocabularyKey CandidateId { get; internal set; }
+        public VocabularyKey AppliedAt { get; internal set; }
+    }
+}
+
+
